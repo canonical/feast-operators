6d9fbe191eb2d5f4e5baa7a66577623192039410
NohaIhab <noha.ihab@canonical.com>
2025-04-16 11:32:46 +0200

    add unit test for ActiveStatus

--- charms/feast-integrator/tests/unit/test_charm.py
+++ charms/feast-integrator/tests/unit/test_charm.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import ops
 import ops.testing as testing
 import pytest
@@ -86,3 +88,40 @@ def test_install_all_relations_added_database_relation_empty(ctx):
     assert state_out.unit_status == ops.WaitingStatus(
         "[offline-store] Waiting for offline-store relation data"
     )
+
+
+@patch("components.database_requirer_component.PostgresRequirerComponent.fetch_relation_data")
+def test_install_database_relations_added(mock_fetch_relation_data, ctx):
+    """Test unit status when all database relations are added with the expected data."""
+    # GIVEN that
+    # * the unit is leader
+    # * database and kubernetes_manifest relations are added
+    # * fetch_relation_data returns the db configuration as expected
+
+    mock_data = {
+        "host": "localhost",
+        "port": 5432,
+        "database": "mydb",
+        "user": "myuser",
+        "password": "mypassword",
+    }
+
+    mock_fetch_relation_data.return_value = mock_data
+
+    relations = [
+        testing.Relation(
+            endpoint="offline-store", interface="postgresql_client", remote_app_data={}
+        ),
+        testing.Relation(endpoint="online-store", interface="postgresql_client"),
+        testing.Relation(endpoint="registry", interface="postgresql_client"),
+        testing.Relation(endpoint="secrets", interface="kubernetes_manifest"),
+        testing.Relation(endpoint="pod-defaults", interface="kubernetes_manifest"),
+    ]
+
+    state_in = State(leader=True, relations=relations)
+
+    # WHEN install fires
+    state_out = ctx.run(ctx.on.install(), state_in)
+
+    # THEN the unit status is set to ActiveStatus
+    assert state_out.unit_status == ops.ActiveStatus()
