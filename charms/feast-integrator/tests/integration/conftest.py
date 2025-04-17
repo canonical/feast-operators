a2681be21271d3f975d894599befc8d588b55617
NohaIhab <noha.ihab@canonical.com>
2025-04-17 11:12:37 +0200

    tests: add integration tests using jubilant

---
+++ charms/feast-integrator/tests/integration/conftest.py
@@ -0,0 +1,31 @@
+# tests/integration/conftest.py
+
+import jubilant
+import pytest
+
+
+@pytest.fixture(scope="module")
+def juju(request: pytest.FixtureRequest):
+    keep_models = bool(request.config.getoption("--keep-models"))
+
+    with jubilant.temp_model(keep=keep_models) as juju:
+        juju.wait_timeout = 10 * 60
+
+        yield juju  # run the test
+
+        if request.session.testsfailed:
+            log = juju.debug_log(limit=1000)
+            print(log, end="")
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--keep-models",
+        action="store_true",
+        default=False,
+        help="keep temporarily-created models",
+    )
+    parser.addoption(
+        "--charm-path",
+        help="Path to charm file for performing tests on.",
+    )
