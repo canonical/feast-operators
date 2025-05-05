e4f70faf7008427ea6807a803a1da309e89eda1e
NohaIhab <noha.ihab@canonical.com>
2025-05-05 09:25:52 +0300

    tests: add option for existing model

--- charms/feast-integrator/tests/integration/conftest.py
+++ charms/feast-integrator/tests/integration/conftest.py
@@ -7,8 +7,10 @@ import pytest
 @pytest.fixture(scope="module")
 def juju(request: pytest.FixtureRequest):
     keep_models = bool(request.config.getoption("--keep-models"))
+    model = request.config.getoption("--model")
 
-    with jubilant.temp_model(keep=keep_models) as juju:
+    if model:
+        juju = jubilant.Juju(model=model)
         juju.wait_timeout = 10 * 60
 
         yield juju  # run the test
@@ -17,6 +19,16 @@ def juju(request: pytest.FixtureRequest):
             log = juju.debug_log(limit=1000)
             print(log, end="")
 
+    else:
+        with jubilant.temp_model(keep=keep_models) as juju:
+            juju.wait_timeout = 10 * 60
+
+            yield juju  # run the test
+
+            if request.session.testsfailed:
+                log = juju.debug_log(limit=1000)
+                print(log, end="")
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -24,6 +36,13 @@ def pytest_addoption(parser):
         action="store_true",
         default=False,
         help="keep temporarily-created models",
+    )
+    parser.addoption(
+        "--model",
+        action="store",
+        help="Juju model to use; if not provided, a new model "
+        "will be created for each test which requires one",
+        default=None,
     )
     parser.addoption(
         "--charm-path",
