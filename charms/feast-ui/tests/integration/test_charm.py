5732463e497fb63d67191620cc2d975d53bf2910
misohu <michal.hucko@canonical.com>
2025-05-27 14:00:44 +0200

    Implement the ui charm

--- charms/feast-ui/tests/integration/test_charm.py
+++ charms/feast-ui/tests/integration/test_charm.py
@@ -81,7 +81,7 @@ def test_deploy_charm(juju: jubilant.Juju, request):
     )
 
     # Doubling the default timeout https://github.com/canonical/jubilant/blob/main/jubilant/_juju.py#L98
-    juju.wait(jubilant.all_active, timeout=6 * 60.0)
+    juju.wait(jubilant.all_active, successes=2, timeout=6 * 60.0)
 
 
 def test_ingress_setup(juju: jubilant.Juju):
@@ -100,7 +100,7 @@ def test_ingress_setup(juju: jubilant.Juju):
 
     juju.integrate(f"{ISTIO_PILOT.charm}:ingress", f"{CHARM_NAME}:ingress")
     # Doubling the default timeout https://github.com/canonical/jubilant/blob/main/jubilant/_juju.py#L98
-    juju.wait(jubilant.all_active, timeout=6 * 60.0)
+    juju.wait(jubilant.all_active, successes=2, timeout=6 * 60.0)
 
 
 def charm_path_from_root(charm_dir_name: str) -> Path:
