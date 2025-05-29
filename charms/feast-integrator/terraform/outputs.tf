0978e712979681460a6af5bc6f1c48af3dd93e0f
NohaIhab <noha.ihab@canonical.com>
2025-05-29 13:07:08 +0300

    terraform: add missing provides output for feast-integrator

--- charms/feast-integrator/terraform/outputs.tf
+++ charms/feast-integrator/terraform/outputs.tf
@@ -2,6 +2,11 @@ output "app_name" {
   value = juju_application.feast_integrator.name
 }
 
+output "provides" {
+  value = {
+    feast_configuration = "feast-configuration",
+  }
+}
 
 output "requires" {
   value = {
