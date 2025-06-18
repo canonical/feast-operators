ff49edc9177a76754336adf0c3100530b17f87c8
NohaIhab <noha.ihab@canonical.com>
2025-06-18 14:39:50 +0300

    chore: update terraform module channel to 0.49/stable

--- charms/feast-ui/terraform/variables.tf
+++ charms/feast-ui/terraform/variables.tf
@@ -7,7 +7,7 @@ variable "app_name" {
 variable "channel" {
   description = "Charm channel"
   type        = string
-  default     = "latest/edge"
+  default     = "0.49/stable"
 }
 
 variable "config" {
