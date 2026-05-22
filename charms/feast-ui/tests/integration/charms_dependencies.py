74290a468ec3c3792d3fe7703c5d2ef15cf34f17
Dario Faccin <dario.faccin@canonical.com>
2026-05-22 09:30:52 +0200

    chore: pin charm dependencies in tests (track/0.49)

--- charms/feast-ui/tests/integration/charms_dependencies.py
+++ charms/feast-ui/tests/integration/charms_dependencies.py
@@ -12,17 +12,17 @@ REGISTRY = CharmSpec(
     charm="postgresql-k8s", channel="14/stable", trust=True, config={"profile": "testing"}
 )
 
-FEAST_INTEGRATOR = CharmSpec(charm="feast-integrator", channel="latest/edge", trust=True)
-METACONTROLLER = CharmSpec(charm="metacontroller-operator", channel="latest/edge", trust=True)
-RESOURCE_DISPATCHER = CharmSpec(charm="resource-dispatcher", channel="latest/edge", trust=True)
-ADMISSION_WEBHOOK = CharmSpec(charm="admission-webhook", channel="latest/edge", trust=True)
+FEAST_INTEGRATOR = CharmSpec(charm="feast-integrator", channel="0.49/edge", trust=True)
+METACONTROLLER = CharmSpec(charm="metacontroller-operator", channel="4.11/edge", trust=True)
+RESOURCE_DISPATCHER = CharmSpec(charm="resource-dispatcher", channel="2.0/edge", trust=True)
+ADMISSION_WEBHOOK = CharmSpec(charm="admission-webhook", channel="1.10/edge", trust=True)
 
 ISTIO_GATEWAY = CharmSpec(
-    charm="istio-gateway", channel="latest/edge", trust=True, config={"kind": "ingress"}
+    charm="istio-gateway", channel="1.28/edge", trust=True, config={"kind": "ingress"}
 )
 ISTIO_PILOT = CharmSpec(
     charm="istio-pilot",
-    channel="latest/edge",
+    channel="1.28/edge",
     trust=True,
     config={"default-gateway": "istio-gateway"},
 )
